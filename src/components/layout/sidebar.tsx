3a5ad261e90a825feb15680fabc2280c438801d0
John Jung <yejoon7154@gmail.com>
2025-11-22 11:44:38 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -98,7 +98,7 @@ export default function AppSidebar() {
 
   return (
     <Sidebar collapsible="icon">
-        <SidebarHeader>
+        <SidebarHeader className="p-4">
             <Link href="/" className="flex items-center space-x-2" onClick={() => handleLinkClick('/')}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6 text-primary">
                     <rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect>
