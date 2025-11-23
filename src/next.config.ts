5df0107e378b26e499ca825d2f73a1b70a8cd202
John Jung <yejoon7154@gmail.com>
2025-11-23 13:30:39 +0000

    Instead of going into offline mode, it still just says "you're offline"

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,5 +1,49 @@
 
 import type {NextConfig} from 'next';
+import path from 'path';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  sw: 'firebase-messaging-sw.js',
+   runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|gif|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        expiration: {
+          maxEntries: 30,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/placehold\.co\/.*/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'placeholder-images',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        },
+      },
+    },
+  ],
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -19,7 +63,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Removed webpack customization to test with Turbopack
+  env: {
+    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version,
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
