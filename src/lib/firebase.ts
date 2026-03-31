08b3c8d71a38ccfd580d7e4c972c1f3fb0c7da63
John Jung <yejoon7154@gmail.com>
2026-03-31 19:58:16 +1000

    Corrected storage bucket configuration to cell-abca4.firebasestorage.app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBjpGl-kwbFgnQ1hGA8dg23K2aGxT1f8jo",
   authDomain: "cell-abca4.firebaseapp.com",
   projectId: "cell-abca4",
-  storageBucket: "cell-abca4.appspot.com",
+  storageBucket: "cell-abca4.firebasestorage.app",
   messagingSenderId: "942477536312",
   appId: "1:942477536312:web:9487c6359a19a4c0e7cacd",
   measurementId: "G-1E3HH6TK1J"
